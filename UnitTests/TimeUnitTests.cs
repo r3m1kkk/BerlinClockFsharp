e9c70025770a417eacb486a0b1ef646a745d8cdb
Remik <contact@creoit.eu>
2016-01-27 23:55:38 +0100

    [BerlinClock] - Time unit tests method names refactored

--- UnitTests/TimeUnitTests.cs
+++ UnitTests/TimeUnitTests.cs
@@ -10,7 +10,7 @@ namespace BerlinClock.UnitTests
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void is_exception_thrown_when_hour_is_negative()
+        public void is_time_exception_thrown_when_hour_is_negative()
         {
             //arrange
             var time = "-10:00:00";
@@ -21,7 +21,7 @@ namespace BerlinClock.UnitTests
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void is_exception_thrown_when_hour_is_not_integer()
+        public void is_time_exception_thrown_when_hour_is_not_integer()
         {
             //arrange
             var time = "a:0:00";
@@ -32,7 +32,7 @@ namespace BerlinClock.UnitTests
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void is_exception_thrown_when_hour_is_greater_than_24()
+        public void is_time_exception_thrown_when_hour_is_greater_than_24()
         {
             //arrange
             var time = "26:0:00";
@@ -43,7 +43,7 @@ namespace BerlinClock.UnitTests
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void is_exception_thrown_when_minute_is_negative()
+        public void is_time_exception_thrown_when_minute_is_negative()
         {
             //arrange
             var time = "10:-20:00";
@@ -54,7 +54,7 @@ namespace BerlinClock.UnitTests
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void is_exception_thrown_when_minute_is_not_integer()
+        public void is_time_exception_thrown_when_minute_is_not_integer()
         {
             //arrange
             var time = "10:z:00";
@@ -65,7 +65,7 @@ namespace BerlinClock.UnitTests
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void is_exception_thrown_when_minute_is_greater_than_59()
+        public void is_time_exception_thrown_when_minute_is_greater_than_59()
         {
             //arrange
             var time = "10:71:00";
@@ -76,7 +76,7 @@ namespace BerlinClock.UnitTests
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void is_exception_thrown_when_second_is_negative()
+        public void is_time_exception_thrown_when_second_is_negative()
         {
             //arrange
             var time = "10:20:-20";
@@ -87,7 +87,7 @@ namespace BerlinClock.UnitTests
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void is_exception_thrown_when_second_is_not_integer()
+        public void is_time_exception_thrown_when_second_is_not_integer()
         {
             //arrange
             var time = "10:20:z";
@@ -98,7 +98,7 @@ namespace BerlinClock.UnitTests
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void is_exception_thrown_when_second_is_greater_than_59()
+        public void is_time_exception_thrown_when_second_is_greater_than_59()
         {
             //arrange
             var time = "10:20:71";
@@ -109,7 +109,7 @@ namespace BerlinClock.UnitTests
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void is_exception_thrown_when_total_time_is_greater_than_24_0_0()
+        public void is_time_exception_thrown_when_total_time_is_greater_than_24_0_0()
         {
             //arrange
             var time = "24:00:01";
@@ -119,7 +119,7 @@ namespace BerlinClock.UnitTests
         }
 
         [TestMethod]
-        public void is_0_0_0_properly_parsed()
+        public void is_time_0_0_0_properly_parsed()
         {
             //arrange
             var time = "0:0:0";
@@ -134,7 +134,7 @@ namespace BerlinClock.UnitTests
         }
 
         [TestMethod]
-        public void is_0_01_0_properly_parsed()
+        public void is_time_0_01_0_properly_parsed()
         {
             //arrange
             var time = "0:01:0";
@@ -149,7 +149,7 @@ namespace BerlinClock.UnitTests
         }
 
         [TestMethod]
-        public void is_14_01_59_properly_parsed()
+        public void is_time_14_01_59_properly_parsed()
         {
             //arrange
             var time = "14:01:59";
@@ -164,7 +164,7 @@ namespace BerlinClock.UnitTests
         }
 
         [TestMethod]
-        public void is_23_59_59_properly_parsed()
+        public void is_time_23_59_59_properly_parsed()
         {
             //arrange
             var time = "23:59:59";
