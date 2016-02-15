d47fcb4fe6b5c340c131a05c0145d52aa04e6d2c
Remik <contact@creoit.eu>
2016-02-15 23:21:48 +0100

    [F# tests] - first test with a friendly name

---
+++ BerlinClock.UnitTests/TimeUnitTests.fs
@@ -0,0 +1,20 @@
+﻿module TimeUnitTests
+
+open Microsoft.VisualStudio.TestTools.UnitTesting;
+open BerlinClock.Classes
+
+[<TestClass>]
+type TimeUnitTests() = 
+    
+    [<TestMethod>]
+    member this.``is time 23:59:59 properly parsed``() = 
+        //arrange
+        let time = "23:59:59"
+        
+        //act
+        let parsedTime = Time.Parse(time)
+
+        //assert
+        Assert.AreEqual(23, parsedTime.Hour)
+        Assert.AreEqual(59, parsedTime.Minute)
+        Assert.AreEqual(59, parsedTime.Second)
