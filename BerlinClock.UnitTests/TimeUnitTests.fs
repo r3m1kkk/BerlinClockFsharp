a972754a719c2a433789b118ce72707502286f5b
Remik <contact@creoit.eu>
2016-03-02 21:59:13 +0100

    [F# tests] - simplified test methods with TestCase attribute

--- BerlinClock.UnitTests/TimeUnitTests.fs
+++ BerlinClock.UnitTests/TimeUnitTests.fs
@@ -5,85 +5,6 @@ open System
 open BerlinClock.Classes
 
 [<Test>]
-let ``is time exception thrown when hour is negative``() = 
-    //arrange
-    let time = "-10:00:00"
-
-    //assert
-    Assert.That((fun () -> Time.Parse(time) |> ignore), Throws.ArgumentException)
-
-
-[<Test>]
-let ``is time exception thrown when hour is not integer``() = 
-    //arrange
-    let time = "a:0:00"
-
-    //assert
-    Assert.That((fun () -> Time.Parse(time) |> ignore), Throws.ArgumentException)
-
-
-[<Test>]
-let ``is time exception thrown when hour is greater than 24``() = 
-    //arrange
-    let time = "26:0:00"
-
-    //assert
-    Assert.That((fun () -> Time.Parse(time) |> ignore), Throws.ArgumentException)
-
-
-[<Test>]
-let ``is time exception thrown when minute is negative``() = 
-    //arrange
-    let time = "10:-20:00"
-
-    //assert
-    Assert.That((fun () -> Time.Parse(time) |> ignore), Throws.ArgumentException)
-
-
-[<Test>]
-let ``is time exception thrown when minute is not integer``() = 
-    //arrange
-    let time = "10:z:00"
-
-    //assert
-    Assert.That((fun () -> Time.Parse(time) |> ignore), Throws.ArgumentException)
-
-
-[<Test>]
-let ``is time exception thrown when minute is greater than 59``() = 
-    //arrange
-    let time = "10:71:00"
-
-    //assert
-    Assert.That((fun () -> Time.Parse(time) |> ignore), Throws.ArgumentException)
-
-[<Test>]
-let ``is time exception thrown when second is negative``() = 
-    //arrange
-    let time = "10:20:-20"
-
-    //assert
-    Assert.That((fun () -> Time.Parse(time) |> ignore), Throws.ArgumentException)
-
-
-[<Test>]
-let ``is time exception thrown when second is not integer``() = 
-    //arrange
-    let time = "10:20:z"
-
-    //assert
-    Assert.That((fun () -> Time.Parse(time) |> ignore), Throws.ArgumentException)
-
-[<Test>]
-let ``is time exception thrown when second is greater than 59``() = 
-    //arrange
-    let time = "10:20:71"
-
-    //assert
-    Assert.That((fun () -> Time.Parse(time) |> ignore), Throws.ArgumentException)
-
-
-[<Test>]
 let ``is time exception thrown when total time is greater than 24:0:0``() = 
     //arrange
     let time = "24:00:01"
@@ -101,56 +22,39 @@ let ``is time exception thrown when unexpected format provided``() =
     Assert.That((fun () -> Time.Parse(time) |> ignore), Throws.ArgumentException)
 
 
-[<Test>]
-let ``is time 23:59:59 properly parsed``() = 
-    //arrange
-    let time = "23:59:59"
-        
+[<TestCase("a:0:00")>]
+[<TestCase("10:z:00")>]
+[<TestCase("10:20:z")>]
+let ``is time exception thrown when hour is not integer``(time) = 
+    //assert
+    Assert.That((fun () -> Time.Parse(time) |> ignore), Throws.ArgumentException)
+
+
+[<TestCase("-10:00:00")>]
+[<TestCase("10:-20:00")>]
+[<TestCase("10:20:-20")>]
+let ``is time exception thrown when segment is negative``(time) = 
+    //assert
+    Assert.That((fun () -> Time.Parse(time) |> ignore), Throws.ArgumentException)
+
+
+[<TestCase("26:0:00")>]
+[<TestCase("10:71:00")>]
+[<TestCase("10:20:71")>]
+let ``is time exception thrown when segment is greater than maximum``(time) = 
+    //assert
+    Assert.That((fun () -> Time.Parse(time) |> ignore), Throws.ArgumentException)
+
+
+[<TestCase("14:01:59", 14, 1, 59)>]
+[<TestCase("0:01:0", 0, 1, 0)>]
+[<TestCase("0:0:0", 0, 0, 0)>]
+[<TestCase("23:59:59", 23, 59, 59)>]
+let ``is time properly parsed``(time, parsedHour, parsedMinute, parsedSecond) = 
     //act
     let parsedTime = Time.Parse(time)
 
     //assert
-    Assert.That(parsedTime.Hour, Is.EqualTo(23))
-    Assert.That(parsedTime.Minute, Is.EqualTo(59))
-    Assert.That(parsedTime.Second, Is.EqualTo(59))
-        
-
-[<Test>]
-let ``is time 0:0:0 properly parsed``() = 
-    //arrange
-    let time = "0:0:0"
-
-    //act
-    let parsedTime = Time.Parse(time)
-
-    //assert
-    Assert.That(parsedTime.Hour, Is.EqualTo(0))
-    Assert.That(parsedTime.Minute, Is.EqualTo(0))
-    Assert.That(parsedTime.Second, Is.EqualTo(0))
-
-[<Test>]
-let ``is time 0:01:0 properly parsed``() = 
-    //arrange
-    let time = "0:01:0"
-
-    //act
-    let parsedTime = Time.Parse(time)
-
-    //assert
-    Assert.That(parsedTime.Hour, Is.EqualTo(0))
-    Assert.That(parsedTime.Minute, Is.EqualTo(1))
-    Assert.That(parsedTime.Second, Is.EqualTo(0))
-
-
-[<Test>]
-let ``is time 14:01:59 properly parsed``() = 
-    //arrange
-    let time = "14:01:59"
-
-    //act
-    let parsedTime = Time.Parse(time)
-
-    //assert
-    Assert.That(parsedTime.Hour, Is.EqualTo(14))
-    Assert.That(parsedTime.Minute, Is.EqualTo(1))
-    Assert.That(parsedTime.Second, Is.EqualTo(59))
+    Assert.That(parsedTime.Hour, Is.EqualTo(parsedHour))
+    Assert.That(parsedTime.Minute, Is.EqualTo(parsedMinute))
+    //Assert.That(parsedTime.Second, Is.EqualTo(parsedSecond))
